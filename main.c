8d51e561f6a594c4ba916c0981cdd790e4abc404
Akash Das <akashdas.ny@gmail.com>
2019-10-16 22:02:34 -0400

    add functions

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ char * outputStat(struct foo example) {
 
 struct foo generateExample() {
   struct foo player;
+  srand(time(NULL));
   int n = rand() % 5;
   if (n == 0) {
     player.s = "Michael Jordan";
@@ -35,6 +36,10 @@ struct foo generateExample() {
   return player;
 }
 
+void modify(struct foo example, int new_a) {
+    example.a = new_a;
+}
+
 int main() {
   struct foo player0;
   struct foo player1;
@@ -43,11 +48,21 @@ int main() {
   player1.a = 25;
   player1.s = "Kobe Bryant";
   printf("%s\n",outputStat(player0));
-  printf("%s\n",outputStat(player1));
+  printf("%s\n\n",outputStat(player1));
 
   struct foo newPlayer = generateExample();
 
-  printf("%s\n",outputStat(newPlayer));
+  printf("The random player generated is...\n");
+  printf("%s\n\n",outputStat(newPlayer));
+
+  struct foo player2;
+  player2.a = 27;
+  player2.s = "Kareem Abdul Jabbar";
+  printf("%s\n", outputStat(player2));
+  printf("He then had a fantastic season with the Bucks! Now ...\n");
+  modify(player2, 30);
+  printf("%s\n", outputStat(player2));
+
 
   return 0;
 }
